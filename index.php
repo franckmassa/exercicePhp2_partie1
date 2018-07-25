25ef27ba7c1aab706891c6930f8c5dadd8ed54db
massa <massafranck@gmail.com>
2018-07-25 19:59:42 +0200

    initial

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+$lastname = 'Dupont';
+$firstname = 'Denis';
+$age = 25;
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice2</title>
+  </head>
+  <body>
+<p>
+    <?php
+echo $lastname.' '.$firstname.' '.$age;
+     ?>
+     </p>
+  </body>
+</html>
